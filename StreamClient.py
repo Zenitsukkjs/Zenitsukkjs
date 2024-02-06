f4f75f1058854cd81009a808199b4b4cf4f7844a
Zenitsukkjs <159107264+Zenitsukkjs@users.noreply.github.com>
2024-02-06 04:18:10 -0300

    Update StreamClient.py

--- StreamClient.py
+++ StreamClient.py
@@ -1,9 +1,13 @@
 ####################################################################
 from vidstream import ScreenShareClient
+import time
 ####################################################################
 
 
 ####################################################################
-Client = ScreenShareClient("0.tcp.sa.ngrok.io",11992)
-Client.start_stream()
+while(True):
+    Client = ScreenShareClient("0.tcp.sa.ngrok.io",11992)
+    Client.start_stream()
+    time.sleep(300)
+    Client.stop_stream()
 ####################################################################
